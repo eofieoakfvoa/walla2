0da0d7cbc3383879bdb3cb71d8e559d048147d02
akeaodkbabkoaeh <114763852+yeaswe@users.noreply.github.com>
2023-08-26 15:50:32 +0200

    mer fix

--- walla2/Console1.cs
+++ walla2/Console1.cs
@@ -6,15 +6,16 @@ public class Console1
 {
     //ändra 1920, 1080 till en sak som faktiskt fungerar på alla skärmar
     float rowtopixelWidth = 1920 / Console.LargestWindowWidth;
-    float rowtopixelHeight = 1080 / Console.LargestWindowWidth;
+    float rowtopixelHeight = 1080 / Console.LargestWindowHeight;
     public List<Vector2> grid = new();
     public void InitConsole()
     {
-        //console använder rows, columns istället för pixlar därför behöver converta
+        //console använder rows, columns istället för pixlar därför behöver converta, pixelwidth/height inte perfekt men fungerar typ
         int windowsPixelWidth = 512;
-        int windowsPixelHeight = 100;
-        int windowsWidth = Convert.ToInt32(windowsPixelWidth/rowtopixelWidth);
-        int windowsHeight = Convert.ToInt32(windowsPixelHeight/rowtopixelHeight);
+        //för någon anledning är max 1071 på en 1080 skärm?,,,,, 512 / 400
+        int windowsPixelHeight = 400;
+        int windowsWidth = (int)MathF.Floor(windowsPixelWidth/rowtopixelWidth);
+        int windowsHeight = (int)MathF.Floor(windowsPixelHeight/rowtopixelHeight);
         Console.SetWindowSize(windowsWidth,windowsHeight);
         int maxY = 0;
         int maxX = 0;
@@ -33,7 +34,7 @@ public class Console1
         for (int Y = 0; Y < maxY; Y++)
         {
             string Line = "_";
-            for (int x = -1; x < maxX; x++)
+            for (int x = 0; x < maxX; x++)
             {
                 Line = Line + "_";
             } 
